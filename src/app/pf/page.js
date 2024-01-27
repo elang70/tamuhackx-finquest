7e62ae86ac6466c725c51a535d1caad1ba087ddd
elang70 <ethanxlangford5@gmail.com>
2024-01-27 15:24:09 -0600

    Bare bones front end for welcome page and levels page

---
+++ src/app/pf/page.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Page = () => {
+    return (
+        <>
+            <div> 
+                <button>Advance to next stage</button>    
+                <p>Money: $0</p>    
+            </div>
+            <div>
+                <h1>FinQuest</h1>
+                <h2>Stage 1</h2>
+                <h3>Journey: Personal Finance</h3>
+                <h4>Name: Bob</h4>
+                <h4>Age: 16</h4>
+                <h4>Status HS</h4>
+                
+            </div>
+            
+            <div>
+                <button>Assets</button>
+                <button>Banking</button>
+                <button>Occupation</button>
+            </div>
+            
+        </>
+    );
+};
+
+export default Page;
