c745573019404c06f36e5d53b4c4bfc1c74c257b
elang70 <ethanxlangford5@gmail.com>
2024-01-27 17:42:55 -0600

    Created initial prompt to appear on first level page

--- src/app/components/initial_prompt.js
+++ src/app/components/initial_prompt.js
@@ -2,12 +2,42 @@
 import React from 'react';
 
 // Define the Profile function component
-function Profile() {
+function Initial() {
     return (
       <>
-        <h1>This is a profile page</h1>
+        <h1>The start of your personal finance journey!</h1>
+        <h2>Here's the basics:</h2>
+        <ul>
+            <li> 
+                In this game, you will advance through 5 main stages of 
+                life ranging from high school to retirement
+            </li>
+            <li>
+                When you advance to each stage, a prompt will appear
+                with decisions you can make regarding your personal 
+                finance
+            </li>
+            <li>
+                Within each stage, you can also make choices based 
+                on your current financial situation such as buying a 
+                house or a car
+            </li>
+            <li>
+                As you go, you will learn more about the basics of 
+                personal finance and good practices to follow
+            </li>
+              
+        </ul>
+        <h2>First Task:</h2>
+        <p>
+            You are 15! You have just started high school and you 
+            want to learn more about personal finance. You decide to
+            open up a bank account but you realize since you're not
+            18, you need a parent to help you. Your mom takes you to
+            a bank and together, you open up your first bank account!
+        </p>
       </>
     );
 };
 
-export default Profile;
+export default Initial;
--- src/app/pf/page.js
+++ src/app/pf/page.js
@@ -1,39 +1,58 @@
 "use client";
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
+import InitialPrompt from '../components/initial_prompt';
 
 const Page = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    useEffect(() => {
+        setIsOpen(true);
+    }, []);
+
+    const closeModal = () => {
+        setIsOpen(false);
+    }
+
     return (
         <>
+            <Popup open={isOpen} closeOnDocumentClick onClose={closeModal}>
+                <InitialPrompt />
+                <button onClick={closeModal}>
+                    Click to open your first bank account and start your financial journey!
+                </button>
+            </Popup>
+
+
             <div> 
                 <button>Advance to next stage</button>    
-                <p>Money: $0</p>    
+                <p>Money: $20</p>    
             </div>
             <div>
                 <h1>FinQuest</h1>
                 <h2>Stage 1</h2>
                 <h3>Journey: Personal Finance</h3>
                 <h4>Name: Bob</h4>
-                <h4>Age: 16</h4>
+                <h4>Age: 15</h4>
                 <h4>Status HS</h4>
                 
             </div>
             
             <div>
             <Popup trigger=
-                {<button> Assets </button>} 
+                {<button> Side Quests </button>} 
                 modal nested>
                 {
                     close => (
                         <div className='modal'>
                             <div className='content'>
-                                {"Content of the popup (to be replaced with a popup component)"}
+                                {""}
                             </div>
                             <div>
                                 <button onClick=
                                     {() => close()}>
-                                        Close modal
+                                        <InitialPrompt />
                                 </button>
                             </div>
                         </div>
